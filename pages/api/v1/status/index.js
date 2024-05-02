96bac16e830eb8f486523c88fd7a062375e51561
Josué Gomes <138222370+josueigs@users.noreply.github.com>
2024-05-02 04:32:38 +0000

    add `database.js` and `.env `files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   //response.status(200).send("API com comportamento adequado");
   //.json já seta o charset utf8
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "API com configuração adequada" });
 }
 
